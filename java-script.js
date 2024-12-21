3423b17ae1a744ac144185548584f910ef897e21
Danjor225 <179250247+Danjor225@users.noreply.github.com>
2024-12-21 15:48:57 +0000

    Add variables with links to containers

---
+++ java-script.js
@@ -0,0 +1,16 @@
+const startingScore = 200
+
+const addNameButton = document.querySelector('#add-button')
+const nameTextbox = document.querySelector('#name-textbox')
+const nameRow = document.querySelector('#name-row-container')
+const remainingScoreRow = document.querySelector('#remaning-score-row-container')
+const subtractRow = document.querySelector('#subtract-row-container')
+
+addNameButton.addEventListener('click', () => {
+
+    // let newName = nameTextbox.value
+
+    // let newNameDiv = document.createElement('div')
+
+
+})
